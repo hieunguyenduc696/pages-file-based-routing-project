02d0e4354a41be8b1763704e989c1dcd8350dfb5
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-21 23:46:05 +0700

    setting up the main pages

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h2>Filtered Events Page</h2>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h2>Event Detail</h2>
+    </div>
+  );
+};
+
+export default EventDetailPage;
