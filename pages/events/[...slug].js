c8810f10656fbed26d2e2c27ea64b1c4bf86769e
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-22 19:06:05 +0700

    extracting data on the Catch-all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,39 @@
+import { getFilteredEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numYear) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
   return (
     <div>
       <h2>Filtered Events Page</h2>
