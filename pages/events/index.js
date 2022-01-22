b96ae32427a7ecca6afbeea6f690baad2b89a276
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-22 17:53:21 +0700

    navigating to the 'Filtered Events' Page programmatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
 
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
